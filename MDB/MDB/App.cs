6b7ec54fdf25277fc49a7bae2001eb3fc14dd23e
StormerCZ <106588912+StormerCZ@users.noreply.github.com>
2022-05-31 11:42:47 +0200

    Add files via upload

---
+++ MDB/MDB/App.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDB
+{
+    public class App
+    {
+        public void Run()
+        {
+
+            List<int> Listik = new List<int>();
+            using var sr = new StreamReader("");
+            using var sw = new StreamWriter("");
+            var line = sr.ReadLine();
+            //trim
+
+            while (line != null) {
+                var splited = line.Split(";");
+                Listik.Add(new oop { oopsplited[0});
+
+           
+
+
+            while(true)
+            {
+                Console.WriteLine("Zadejte rok"); var inp = int.Parse(Console.ReadLine());
+                var count = 0;
+                if (inp == 0)
+                {
+                    break;
+                }
+
+                foreach(var items in Listik)
+                {
+                    if (items == inp)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+
+    class oop
+        {
+            public string Name { get; set; }
+            public int Year { get; set; }
+            public string Author { get; set; }
+        }
+    }
+}
---
+++ MDB/MDB/items.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDB
+{
+    public class items
+
+
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string Author { get; set; }
+        
+    }
+}
